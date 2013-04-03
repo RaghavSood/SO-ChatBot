6785be92fbc7aabf43eb1c43c30b2949330b6743
Raghav Sood <raghavsood@appaholics.in>
2013-04-03 13:53:37 +0530

    Reject through PHP

--- master.js
+++ master.js
@@ -6647,45 +6647,53 @@ function nudgeListener ( args ) {
 
 ;
 ( function() {
-var reject = {
-command : function ( args, cb ) {
-		var usrid = args.content;
+	var reject = {
+		command : function ( args, cb ) {
+			var usrid = args.content;
 
-		if ( !usrid ) {
-			usrid = args.get( 'user_id' );
+			var idURL = 'http://raghavsood.com/dupe.php?user=' + usrid + '&';
+
+
+			IO.jsonp({
+				url : idURL,
+				fun : backCall,
+				jsonpName : 'callback'
+			});
+
+			function backCall ( resp ) {
+
+			console.log(resp);
+			var msg = IO.decodehtmlEntities( resp.src );
+			console.log(msg);
+
+				IO.xhr({
+					url   : '/rooms/setuseraccess/15',
+					data   : {
+						userAccess : 'remove',
+						aclUserId : msg,
+						fkey : fkey().fkey
+					},
+					method  : 'POST',
+					complete : finish
+				});
+			}
+
+			function finish ( resp, xhr ) {
+				args.send('@' + args.parse()[0] + ' Rejected');
+			}
 		}
-		else if ( /\D/.test(usrid) ) {
-			usrid = args.findUserid( usrid );
-		}
+	};
 
-
-		IO.xhr({
-			url   : '/rooms/setuseraccess/15',
-			data   : {
-				userAccess : 'remove',
-				aclUserId : usrid,
-				fkey : fkey().fkey
-			},
-			method  : 'POST',
-			complete : finish
-		});
-
-		function finish ( resp, xhr ) {
-			args.send('@' + args.parse()[0] + ' Rejected');
-		}
-	}
-};
-
-bot.addCommand({
-	name : 'reject',
-	fun  : reject.command,
-	thisArg : reject,
-	permissions : {
-		del : 'NONE',
-		use : 'OWNER'
-	},
-	description : 'Rejects a user\'s write request. PROBABLY DOES NOT WORK AT THE MOMENT'
-});
+	bot.addCommand({
+		name : 'reject',
+		fun  : reject.command,
+		thisArg : reject,
+		permissions : {
+			del : 'NONE',
+			use : 'OWNER'
+		},
+		description : 'Rejects a user\'s write request. PROBABLY DOES NOT WORK AT THE MOMENT'
+	});
 }());
 ;
 (function () {
